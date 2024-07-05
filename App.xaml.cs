c87523c6614ce985c4949f1975e11637e0068c9a
EvelynMorales07 <evemorales.ar@gmail.com>
2024-07-05 16:51:26 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using emoralesS5.Utils;
+
+namespace emoralesS5
+{
+    public partial class App : Application
+    {
+        public static PersonaRepositorio personRepo {  get; set; }
+
+        public App(PersonaRepositorio person)
+        {
+            InitializeComponent();
+
+            MainPage = new Views.vHome();
+            personRepo = person;
+        }
+    }
+}
